4a4740d34fd378f98ee99be56599a147fc5c9438
Penny5837 <pcfons@gmail.com>
2024-08-30 23:48:57 -0500

    This is the first draft of my first project
    Onto big and bright things, stay tuned to see my coding progress, and enjoy what I hav e created ;D

---
+++ update.bat
@@ -0,0 +1,16 @@
+@echo off
+title Penny's Updater Script
+rem This script will automatically update all the applications on your computer with one button press
+Echo This script will automatically update all the applications on your computer with one button press
+pause
+Echo Update process opening and starting
+rem this next command gets elevated access
+pushd "%~dp0"
+rem if it doesnt work, you have to press yes everytime an app updates
+echo The apps may require you to press Yes as admin to update
+pause
+rem this next command starts powershell and uses the base windows system to update all the apps
+start powershell winget update -h --all
+Echo Update process open and running
+Echo This window will now be able to be closed
+pause
