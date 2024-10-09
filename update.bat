2fb744753322da57b93bca8a5372c24d93a6f978
Penny5837 <pcfons@gmail.com>
2024-10-09 04:36:48 -0400

    Update update.bat
    making some minor changes to the way the script runs to remove some unnecessary inputs as well as fixing minor grammar issues

--- update.bat
+++ update.bat
@@ -1,14 +1,13 @@
 @echo off
 title Penny's Updater Script
-rem This script will automatically update all the applications on your computer with one button press
-Echo This script will automatically update all the applications on your computer with one button press
+rem This script will automatically update all the applications
+Echo This script will automatically update all the applications
 pause
 Echo Update process opening and starting
-rem this next command gets elevated access
+rem this next command tries to get elevated access
 pushd "%~dp0"
 rem if it doesnt work, you have to press yes everytime an app updates
 echo The apps may require you to press Yes as admin to update
-pause
 rem this next command starts powershell and uses the base windows system to update all the apps
 start powershell winget update -h --all  --accept-package-agreements --accept-source-agreements
 Echo Update process open and running
